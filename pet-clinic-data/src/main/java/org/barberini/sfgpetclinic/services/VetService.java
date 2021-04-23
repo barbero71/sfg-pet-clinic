d369d75433b2a0ae4665a5b07470e8af45066dfc
barbero71 <barbero71@gmail.com>
2021-04-23 12:54:02 +0200

    Refactoring to Common CRUD Interface. Closes #12

--- pet-clinic-data/src/main/java/org/barberini/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/org/barberini/sfgpetclinic/services/VetService.java
@@ -2,11 +2,6 @@ package org.barberini.sfgpetclinic.services;
 
 import org.barberini.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
